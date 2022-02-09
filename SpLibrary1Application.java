1b7f1957e679be2b7f028c121fa24a6a4a42bdf1
BanndB007 <bdstudy07@gmail.com>
2022-02-09 10:52:29 +0000

    Initial commit

---
+++ SpLibrary1Application.java
@@ -0,0 +1,13 @@
+package com.qa.splibrary1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLibrary1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpLibrary1Application.class, args);
+	}
+
+}
